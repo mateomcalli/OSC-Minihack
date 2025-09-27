03d13830591dae709188583cde681061c928dbb0
00adrn <adrianvez11@gmail.com>
2025-09-27 19:55:47 -0400

    Dwolla connections succes!!! Only a few liters of blood, sweat, and tears!

---
+++ backend/src/test.ts
@@ -0,0 +1,17 @@
+import type {CustomerData} from './types/types.ts';
+
+const testCustomer: CustomerData = {
+    firstName: "John",
+    lastName: "Doe",
+    email: "joe@gmail.com",
+    type : "personal",
+    address1: "123 Main St",
+    city: "Miami",
+    state: "FL",
+    postalCode: "33126",
+    dateOfBirth: "1990-01-01",
+    ssn: "6789"
+}
+
+export {testCustomer}
+
