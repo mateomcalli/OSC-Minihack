66dcfcde615490cf379c13a903959713dc6a2722
mateomcallister <mateomcallisterschool@gmail.com>
2025-09-27 14:05:47 -0400

    mongodb setup

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,19 @@
-import "dotenv/config"
+import 'dotenv/config'
+import mongoose from 'mongoose';
 import express from 'express';
+
 const app = express();
-const port = 3000;
+const url = process.env.MONGO_URI
+
+mongoose.connect(url)
+	.then(() => console.log('Connected to MongoDB.'))
+	.catch(error => console.log('Error connecting to Mongo: ', error.message))
 
 app.get('/', (req, res) => {
-    res.send('');
+  res.send('');
 });
 
+const port = 3000;
 app.listen(port, () => {
-    console.log(`Backend listening on ${port}`)
+  console.log(`Backend listening on ${port}`)
 });
