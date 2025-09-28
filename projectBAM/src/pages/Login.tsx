5078eb8d087c53bcce44a3c495cc1b51ed98418b
00adrn <adrianvez11@gmail.com>
2025-09-27 22:30:38 -0400

    mateo update

--- projectBAM/src/pages/Login.tsx
+++ projectBAM/src/pages/Login.tsx
@@ -1,35 +1,56 @@
+import { Link } from "react-router-dom"
+import { useState } from "react"
 import Footer from "../components/Footer"
 
+interface LoginData {
+  email: string
+  password: string
+}
+
 const Login = () => {
-    return (
-      <div className="w-screen min-h-screen flex flex-col bg-[#EDF1F3] red">
-        <div className="flex-grow flex items-center justify-center px-8 py-24">
-          <div className="absolute top-0 left-0 w-full h-32 bg-gradient-to-b from-primary/60 to-transparent backdrop-blur-md rounded-b-3xl"></div>
-          <div className="relative z-1 flex flex-col">
-            <div className="flex flex-col">
-              <img src="logo.png" className="w-[64px] h-[70px] mb-7" />
-              <h1 className="font-default text-gray-500 text-4xl">Log In</h1>
-            </div>
-            <div className="mt-7" id="spacer-for-input-fields">
-              <input
-                type="email"
-                placeholder="E-mail Address"
-                className="w-full h-12 bg-white rounded-2xl px-5 mb-5 text-gray-700 placeholder-gray-400 focus:outline-none"
-              />
-              <input
-                type="password"
-                placeholder="Password"
-                className="w-full h-12 bg-white rounded-2xl px-5 mb-5 text-gray-700 placeholder-gray-400 focus:outline-none"
-              />
-              <button className="w-full h-14 bg-primary rounded-2xl flex items-center justify-center px-5 mb-5 mt-10 text-white font-medium">
-                Log in
-              </button>
-            </div>
-          </div>
+  const [loginData, setLoginData] = useState<LoginData>({
+    email: '',
+    password: ''
+  })
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setLoginData(prev => ({
+      ...prev,
+      [event.target.name]: event.target.value
+    }))
+    console.log(loginData)
+  }
+
+  return (
+    <>
+      <div className="relative z-1 flex flex-col">
+        <div className="flex flex-col">
+          <img src="logo.png" className="w-[64px] h-[70px] mb-7" />
+          <h1 className="font-default text-gray-500 text-4xl">Log In</h1>
         </div>
-      <Footer blurb="Need to create an account?" linkPath="/signup" linkText="Sign up"/>
-    </div>
-    )
+        <div className="mt-7" id="spacer-for-input-fields">
+          <input
+            onChange={handleChange}
+            type="email"
+            name="email"
+            placeholder="E-mail Address"
+            className="w-full h-12 bg-white rounded-2xl px-5 mb-5 text-gray-700 placeholder-gray-400 focus:outline-none"
+          />
+          <input
+            onChange={handleChange}
+            type="password"
+            name="password"
+            placeholder="Password"
+            className="w-full h-12 bg-white rounded-2xl px-5 mb-5 text-gray-700 placeholder-gray-400 focus:outline-none"
+          />
+          <Link to='/home' className="w-full h-14 bg-primary rounded-2xl flex items-center justify-center px-5 mb-5 mt-10 text-white font-medium">
+            Log in
+          </Link>
+        </div>
+      </div>
+      <Footer blurb="Don't have an account yet?" linkText="Sign up" linkPath="/"/>
+    </>
+  )
 }
 
 export default Login
